072e2fdf95d2cca087a9c6b0e2d3f70b1d72997f
Qian <pandachain731@gmail.com>
2017-02-04 11:59:19 -0800

    add Header

---
+++ src/components/common/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link, IndexLink } from 'react-router';
+
+const Header = () => {
+  return (
+    <nav>
+      <IndexLink to="/" activeClassName="active">Home</IndexLink>
+      {" | "}
+      <Link to="signup">Sign Up/Log In</Link>
+    </nav>
+  )
+};
+
+export default Header
